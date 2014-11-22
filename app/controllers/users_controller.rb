17dc3829759843a361a731e56905b39ce6f91df4
Martin Payne <martinrpayneuk+git@gmail.com>
2014-11-22 13:14:25 +0000

    More cors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -179,7 +179,7 @@ class UsersController < ApplicationController
     if request.method.downcase == 'options'
       headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, Content-Type, X-Prototype-Version'
       headers['Access-Control-Max-Age'] = '1728000'
       render :text => '', :content_type => 'text/plain'
     end
